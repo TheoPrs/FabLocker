efc2eeb6477409aadb9987ceb281ed702b2fba2a
theyo11 <perustheo2@gmail.com>
2024-01-10 16:51:58 +0100

    modification branche

---
+++ fablocker/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FabLocker'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment(0, -0.85),
+              child: Text(
+                'Bienvenue sur votre application FabLocker !',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 16.0,width: 50), // Ajoute un espace entre les zones de texte
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nom d\'utilisateur',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0,width: 50), // Ajoute un espace entre les zones de texte
+            TextField(
+              obscureText: true, // Pour masquer le texte (mot de passe)
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0), // Ajoute un espace entre les zones de texte
+            ElevatedButton(
+              onPressed: () {
+                // Action à effectuer lors de l'appui sur le bouton
+                print('Bouton appuyé!');
+              },
+              child: Text('Se connecter'),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color(0xFF3C2A53),
+    );
+  }
+}
