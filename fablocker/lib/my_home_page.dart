1e489d213cf0caa254db509cfc8c0f9223e677f8
theyo11 <perustheo2@gmail.com>
2024-01-19 13:49:49 +0100

    Modification d'input

--- fablocker/lib/my_home_page.dart
+++ fablocker/lib/my_home_page.dart
@@ -1,112 +1,109 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key?key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    usernameController.dispose();
-    passwordController.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.inactive ||
-        state == AppLifecycleState.detached) {
-      // App est inactive ou détachée, vous pouvez effectuer des actions de nettoyage ici si nécessaire.
-      usernameController.dispose();
-      passwordController.dispose();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: const Text('FabLocker'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Align(
-              alignment: Alignment(0, -0.85),
-              child: Text(
-                'Bienvenue sur votre application FabLocker !',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'), // Ajuster le chemin de l'image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Align(
+                alignment: Alignment(0, -0.85),
+                child: Text(
+                  'Bienvenue sur votre application FabLocker !',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-                height: 20.0,
-                width: 50), // Ajoute un espace entre les zones de texte
-            SizedBox(
-              height: 50,
-              width: 440,
-              child: TextField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Nom d\'utilisateur',
-                  labelStyle:
-                      TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                  border: OutlineInputBorder(),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                height: 50,
+                width: 440,
+                child: TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nom d\'utilisateur',
+                    labelStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                    border: OutlineInputBorder(),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
-                style: const TextStyle(
-                    color: Colors.white), // Ajouté pour le texte en blanc
               ),
-            ),
-            const SizedBox(height: 20.0), // Ajoute un espace entre les zones de texte
-            SizedBox(
-              height: 50,
-              width: 440,
-              child: TextField(
-                controller: passwordController,
-                obscureText: true, // Pour masquer le texte (mot de passe)
-                decoration: const InputDecoration(
-                  labelText: 'Mot de passe',
-                  labelStyle: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255)),
-                  border: OutlineInputBorder(),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                height: 50,
+                width: 440,
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Mot de passe',
+                    labelStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                    border: OutlineInputBorder(),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
-                style: const TextStyle(
-                    color: Colors.white), // Ajouté pour le texte en blanc
               ),
-            ),
-            const SizedBox(height: 16.0), // Ajoute un espace entre les zones de texte
-            ElevatedButton(
-              onPressed: () {
-                String username = usernameController.text;
-                String password = passwordController.text;
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  String username = usernameController.text;
+                  String password = passwordController.text;
 
-                print('Nom d\'utilisateur : $username');
-                print('Mot de passe : $password');
-              },
-              child: const Text('Se connecter'),
-            ),
-          ],
+                  if (username.isEmpty|| password.isEmpty) {  
+                      ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Veuillez bien entrer un nom d\'utilisateur ainsi qu\'un mot de passe !'),
+                      ),
+                    );
+                    // Si mdp et username correct rediriger vers la page suivante
+                  } else if(username=='test' && password=='test') {
+                    //Vérifier si l'utilsateur existe
+                  }
+                  else{
+                      ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Nom d\'utilisateur ou mot de passe incorrect.'),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Se connecter'),
+              ),
+            ],
+          ),
         ),
       ),
-      backgroundColor: const Color(0xFF3C2A53),
     );
   }
 }
