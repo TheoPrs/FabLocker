b759e14545e88bc4d45326fbc3c2fea96969fbf3
ArthurWyon <arthur.wyon@numericable.fr>
2024-01-22 09:48:39 +0100

    Ajout d'un historique pour les admins

--- fablocker/lib/Adminpage.dart
+++ fablocker/lib/Adminpage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AdminPage extends StatelessWidget {
   const AdminPage({Key? key}) : super(key: key);
 
-  void _showCasierOptions(BuildContext context, int index, GlobalKey key) async {
+  void _showCasierOptions(BuildContext context, int index, GlobalKey key, bool isAdmin) async {
     final RenderBox renderBox = key.currentContext?.findRenderObject() as RenderBox;
     final Offset offset = renderBox.localToGlobal(Offset.zero);
     final RelativeRect position = RelativeRect.fromLTRB(
@@ -13,23 +13,35 @@ class AdminPage extends StatelessWidget {
       offset.dy,
     );
 
+
+    List<PopupMenuEntry<String>> menuItems = [
+      const PopupMenuItem(
+        value: 'ouvrir',
+        child: Text('Ouvrir'),
+      ),
+      const PopupMenuItem(
+        value: 'informations',
+        child: Text('Informations'),
+      ),
+    ];
+
+
+    if (isAdmin) {
+      menuItems.add(
+        const PopupMenuItem(
+          value: 'historique',
+          child: Text('Historique'),
+        ),
+      );
+    }
+
     final selection = await showMenu(
       context: context,
-      position: position, // Utilisez la position calculée ici
-      items: [
-        const PopupMenuItem(
-          value: 'ouvrir',
-          child: Text('Ouvrir'),
-        ),
-        const PopupMenuItem(
-          value: 'informations',
-          child: Text('Informations'),
-        ),
-        // Ajoutez d'autres options ici
-      ],
+      position: position,
+      items: menuItems,
     );
 
-    // Effectuez des actions en fonction de l'option sélectionnée
+ 
     switch (selection) {
       case 'ouvrir':
         // Logique pour ouvrir le casier
@@ -38,6 +50,10 @@ class AdminPage extends StatelessWidget {
       case 'informations':
         // Logique pour afficher les informations
         break;
+      case 'historique':
+        // Logique pour ouvrir l'historique
+        print('Ouvrir historique pour casier $index');
+        break;
       default:
         print('Aucune action');
     }
@@ -45,6 +61,8 @@ class AdminPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isAdmin = true; 
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Page Admin'),
@@ -60,19 +78,19 @@ class AdminPage extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4, // Nombre de colonnes
-          crossAxisSpacing: 10.0, // Espace horizontal entre les cases
-          mainAxisSpacing: 10.0, // Espace vertical entre les cases
-          childAspectRatio: 1, // Ratio pour des cases carrées
+          crossAxisCount: 4,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 1,
         ),
-        itemCount: 16, // Nombre total de cases
+        itemCount: 16,
         itemBuilder: (context, index) {
-          // Créez une GlobalKey pour chaque élément de la grille
+          // Créer une GlobalKey pour chaque élément de la grille.
           final GlobalKey itemKey = GlobalKey();
 
           return InkWell(
-            key: itemKey, // Attribuez la clé ici
-            onTap: () => _showCasierOptions(context, index, itemKey),
+            key: itemKey,
+            onTap: () => _showCasierOptions(context, index, itemKey, isAdmin),
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -83,8 +101,8 @@ class AdminPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Casier : $index', // Assurez-vous d'utiliser la variable index ici
-                      style: TextStyle(
+                      'Casier : $index',
+                      style: const TextStyle(
                         color: Colors.black,
                       ),
                     ),
